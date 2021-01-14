085fb9fffb1bee14c8eb29a856571af8e0f36c6c
Angel Leung <angelhleung@gmail.com>
2021-01-13 23:39:58 -0800

    Completed convertHoursToMinutes function definition

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log("addTwoNumbers Exercise:", addTwoNumbersResult);
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log("convertHoursToMinutes:", convertHoursToMinutesResult);
