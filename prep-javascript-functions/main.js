cb3f5e3af35f99cdb1f1cca5aea17bfcabe1903a
Angel Leung <angelhleung@gmail.com>
2021-01-13 23:45:52 -0800

    Completed personalizeGreeting function definition

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -6,8 +6,15 @@ function convertHoursToMinutes(number) {
   return number * 60;
 }
 
+function personalizeGreeting(name) {
+  return "Hello " + name + "!";
+}
+
 var addTwoNumbersResult = addTwoNumbers(2, 2);
 console.log("addTwoNumbers Exercise:", addTwoNumbersResult);
 
 var convertHoursToMinutesResult = convertHoursToMinutes(2);
-console.log("convertHoursToMinutes:", convertHoursToMinutesResult);
+console.log("convertHoursToMinutes Exercise:", convertHoursToMinutesResult);
+
+var personalizeGreetingResult = personalizeGreeting("World");
+console.log("personalizeGreeting Exercise:", personalizeGreetingResult);
