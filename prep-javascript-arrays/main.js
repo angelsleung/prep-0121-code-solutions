4a73075807d03123642171f18a7797ea1c026e90
Angel Leung <angelhleung@gmail.com>
2021-01-14 20:42:04 -0800

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,25 @@
+var languagesArray = ["html", "css", "javascript", "react", "node.js"];
+console.log("languagesArray:", languagesArray);
+console.log("languagesArray length:", languagesArray.length);
+
+languagesArray.push("C#");
+languagesArray.push("php");
+console.log("Updated languagesArray:", languagesArray);
+
+languagesArray.pop();
+console.log("Last index removed:", languagesArray);
+
+languagesArray.unshift("C++");
+console.log("Added element to front:", languagesArray);
+
+languagesArray.shift();
+console.log("Removed first element:", languagesArray);
+
+var thirdElement = languagesArray[2];
+console.log("3rd element in languagesArray:", thirdElement);
+
+var arrayLength = languagesArray.length;
+console.log("The final length of the array is", arrayLength);
+
+var lastItem = languagesArray[arrayLength - 1];
+console.log("The last item in the array is", lastItem);
